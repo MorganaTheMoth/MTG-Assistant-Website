28dc2b2e44107017b7dfec546b7e2a6440296304
Andrew Cox <andrew.cox01110@gmail.com>
2023-12-11 02:41:56 +1100

    working on having some pages, they dont work and i dont know why

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./Components/home/home.component";
+import {DevHelpComponent} from "./Components/dev-help/dev-help.component";
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+	  {path:'devHelp', component: DevHelpComponent},
+	  {path:'home', component: HomeComponent, pathMatch:"full"},
+	  {path:"**", redirectTo:'home'}
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './Components/HomeGroup/home/home.component';
-import { MainPageSuperComponent } from './Components/MainPageGroup/main-page-super/main-page-super.component';
-import { DevHelpComponent } from './dev-help/dev-help.component';
+import {HomeComponent} from "./Components/home/home.component";
+import {DevHelpComponent} from "./Components/dev-help/dev-help.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MainPageSuperComponent,
     DevHelpComponent
   ],
   imports: [
