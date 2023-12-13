aa1738b179d213b50aba9c10a771aaeb0c0f0d35
Kurtis FR <deleriace@gmail.com>
2023-12-13 16:01:32 +1100

    Added press and hold function to buttons

--- src/app/player-info/player-info.component.ts
+++ src/app/player-info/player-info.component.ts
@@ -1,18 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { interval, Subject } from 'rxjs';
+import { takeUntil, takeWhile } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-player-info',
-  standalone: false,
-  templateUrl: './player-info.component.html',
-  styleUrl: './player-info.component.css'
+	selector: 'app-player-info',
+	standalone: false,
+	templateUrl: './player-info.component.html',
+	styleUrl: './player-info.component.css',
 })
 export class PlayerInfoComponent {
-   lifeTotal: number = 40
+	lifeTotal: number = 40;
+	privateChangeInterval: number = 125;
 
-   increment = () => {
-    this.lifeTotal += 1
-   }
-   decrement = () => {
-    this.lifeTotal -= 1
-   }
+	private destroy$: Subject<void> = new Subject<void>();
+	private counting = false;
+
+	startIncrementing() {
+		this.counting = true;
+		this.lifeTotal++;
+		interval(this.privateChangeInterval)
+			.pipe(takeWhile(() => this.counting),
+			takeUntil(this.destroy$))
+			.subscribe(() => {
+				this.lifeTotal++;
+			});
+	}
+
+	stopDecrementing() {
+		this.counting = false;
+		this.destroy$.next();
+		this.destroy$.complete();
+	}
+
+	startDecrementing() {
+		this.counting = true;
+		this.lifeTotal--;
+		interval(this.privateChangeInterval)
+			.pipe(takeWhile(() => this.counting),
+			takeUntil(this.destroy$))
+			.subscribe(() => {
+				this.lifeTotal--;
+			});
+	}
+
+	stopIncrementing() {
+		this.counting = false;
+		this.destroy$.next();
+		this.destroy$.complete();
+	}
+
+
+	ngOnDestroy() {
+		this.stopIncrementing();
+		this.stopDecrementing();
+	  }
 }
