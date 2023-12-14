3b20b711f13b582195e726830960bb2230e2a9b9
Andrew Cox <andrew.cox01110@gmail.com>
2023-12-14 16:38:54 +1100

    trying my hardest to make it so we can change the amount of player in the game

--- src/app/Components/life-tracker/life-tracker.component.ts
+++ src/app/Components/life-tracker/life-tracker.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PlayerInfoComponent } from 'src/app/player-info/player-info.component';
+import {toArray} from "rxjs";
 
 @Component({
   selector: 'app-life-tracker',
@@ -9,4 +10,17 @@ import { PlayerInfoComponent } from 'src/app/player-info/player-info.component';
 })
 export class LifeTrackerComponent {
 
+	public top_row_cells: number = 2;
+	public top_row_array : any;
+	public bottom_row_array :any;
+	public bottom_row_cells: number = 2;
+	constructor() {
+		this.bottom_row_array = this.GenItteratableArray(this.bottom_row_cells);
+		this.top_row_array = this.GenItteratableArray(this.top_row_cells);
+	}
+	//plea se understand i hate everything about this
+	GenItteratableArray(i : number) : any {
+		console.log(i)
+		return new Array(i).fill(0);
+	}
 }
